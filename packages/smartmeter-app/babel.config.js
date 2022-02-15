d4ad7a99131ba6f5e98727b3bc0eb01c8d2311f9
Josh Balfour <josh@joshbalfour.co.uk>
2022-02-15 18:12:57 +0000

    more

--- packages/smartmeter-app/babel.config.js
+++ packages/smartmeter-app/babel.config.js
@@ -2,5 +2,11 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      ['@babel/plugin-proposal-decorators', { legacy: true }],
+      'babel-plugin-transform-typescript-metadata',
+      ['@babel/plugin-proposal-class-properties', { loose: true }],
+      ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
+    ],
   };
 };
